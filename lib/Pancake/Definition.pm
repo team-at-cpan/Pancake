47810525dc940ba35abc5cd973704e12c4879474
Tom Molesworth <tom@perlsite.co.uk>
2015-02-28 17:57:42 +0000

    Generate class definitions
    git-svn-id: http://entitymodel.com/svn/trunk/Pancake@3038 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

---
+++ lib/Pancake/Definition.pm
@@ -0,0 +1,71 @@
+package Pancake::Definition;
+
+use strict;
+use warnings;
+
+use Log::Any qw($log);
+
+use Module::Load;
+use Data::Dumper;
+
+sub import {
+	my ($class, $def, %args) = @_;
+
+	my %loader;
+	my $pkg = caller;
+	for my $k (keys %$def) {
+		my $details = $def->{$k};
+		$details = { type => $details } unless ref $details;
+		my $code;
+		my %collection_class_for = (
+			UnorderedMap => 'Adapter::Async::UnorderedMap::Hash',
+			OrderedList  => 'Adapter::Async::OrderedList::Array',
+		);
+
+		if(my $type = $details->{collection}) {
+			my $collection_class = $collection_class_for{$type} // die "unknown collection $type";
+			++$loader{$collection_class};
+			$log->tracef("%s->%s collection: %s", $pkg, $k, $type);
+			++$loader{$_} for grep /::/, map $class->type_expand($_), @{$details}{qw(key item)};
+			$code = sub {
+				my $self = shift;
+				die "no args expected" if @_;
+				$self->{$k} //= $collection_class->new;
+			}
+		} else {
+			my $type = $class->type_expand($details->{type} // die "unknown type in package $pkg - " . Dumper($def));
+			++$loader{$type} if $type =~ /::/;
+
+			$log->tracef("%s->%s scalar %s", $pkg, $k, $type);
+			$code = sub {
+				my ($self) = shift;
+				return $self->{$k} unless @_;
+				$self->{$k} = shift;
+				return $self
+			}
+		}
+
+		{ # Apply the method
+			no strict 'refs';
+			*{$pkg . '::' . $k} = $code;
+			*{$pkg . '::new'} = sub {
+				my ($class) = shift; bless { @_ }, $class
+			}
+		}
+	}
+
+	for(sort keys %loader) {
+		$log->tracef("Loading %s for %s", $_, $pkg);
+		Module::Load::load($_) 
+	}
+}
+
+sub type_expand {
+	my ($class, $type) = @_;
+	return unless defined $type;
+	$type = 'Pancake::Model' . $type if substr($type, 0, 2) eq '::';
+	$type
+}
+
+1;
+
